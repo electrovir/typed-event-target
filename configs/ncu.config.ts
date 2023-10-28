fd1e34792a5a7bb43addd0a9890443f752820635
electrovir <electrovir@users.noreply.github.com>
2023-10-28 11:48:15 +0000

    update configs

--- configs/ncu.config.ts
+++ configs/ncu.config.ts
@@ -2,9 +2,6 @@ import {RunOptions} from 'npm-check-updates';
 import {baseNcuConfig} from 'virmator/dist/compiled-base-configs/base-ncu';
 
 export const ncuConfig: RunOptions = {
-    color: true,
-    upgrade: true,
-    root: true,
     // exclude these
     reject: [
         ...baseNcuConfig.reject,
