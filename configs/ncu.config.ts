c2c9c14c03e4992740882fea737673d5d255e405
electrovir <electrovir@users.noreply.github.com>
2023-11-20 17:58:17 +0000

    [patch] update deps and configs

--- configs/ncu.config.ts
+++ configs/ncu.config.ts
@@ -2,6 +2,7 @@ import {RunOptions} from 'npm-check-updates';
 import {baseNcuConfig} from 'virmator/dist/compiled-base-configs/base-ncu';
 
 export const ncuConfig: RunOptions = {
+    ...baseNcuConfig,
     // exclude these
     reject: [
         ...baseNcuConfig.reject,
